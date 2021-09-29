35974f8c6b4ef46a0a4cb10495d5d4ef6ea89f62
yuga0019 <yugastar.19@gmail.com>
2021-09-29 21:18:36 +0530

    master

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,30 @@
+package org.phone;
+
+public class PhoneInfo {
+//Method
+	private void phoneName() {
+		System.out.println("Iphone 8");
+	}
+	private void phoneMieiNum() {
+		System.out.println("34 XXXXXXXXX 12");
+	}
+	private void Camera() {
+		System.out.println("7 & 12 MP Front & Back Camera");
+	}
+	private void storage() {
+		System.out.println("64 GB");
+	}
+	private void osName() {
+		System.out.println("ios 13.6.1");
+	}
+	//Execute
+	public static void main(String[] args) {
+		PhoneInfo c = new PhoneInfo();
+				c.phoneName();
+		c.phoneMieiNum();
+		c.Camera();
+		c.storage();
+		c.osName();
+	}
+
+}
